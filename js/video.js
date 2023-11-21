628b7e64065d71fc903608485aa417b8d95beca1
mengyun1221 <143959537+mengyun1221@users.noreply.github.com>
2023-11-20 22:52:07 -0500

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,74 @@
+var video;
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+ 	console.log("Play Video");
+ });
+
+document.addEventListener("DOMContentLoaded", (event) => {
+
+    let video = document.getElementById("player1");
+    video.autoplay = false;
+    video.loop = false;
+
+document.getElementById("play").addEventListener("click", playVideo);
+function playVideo() {
+    video.play();
+    let volumeInfo = Math.round(video.volume * 100);
+    document.getElementById("volume").textContent =  volumeInfo + "%";
+}
+
+document.getElementById("pause").addEventListener("click", pauseVideo);   
+function pauseVideo() {
+    video.pause();
+}
+
+document.getElementById("slower").addEventListener("click", function() {
+    video.playbackRate *= 0.9;
+    console.log("New speed is " + video.playbackRate.toFixed(2));
+});
+
+document.getElementById("faster").addEventListener("click", function() {
+    video.playbackRate /= 0.9;
+    console.log("New speed is " + video.playbackRate.toFixed(2));
+});
+
+document.getElementById("skip").addEventListener("click", function() {
+    if (video.currentTime + 10 < video.duration) {
+        video.currentTime =  video.currentTime + 10;
+    } else {
+        video.currentTime = 0; 
+    }
+    console.log("Current location is " + video.currentTime);
+});
+
+document.getElementById("mute").addEventListener("click", function() {
+    if (video.muted) {
+        video.muted = false;
+        document.getElementById("mute").innerText = "Mute";
+    } else {
+        video.muted = true;
+        document.getElementById("mute").innerText = "Unmute";
+    }
+});
+
+document.getElementById("slider").addEventListener("input", function() {
+    let sliderValue = document.getElementById("slider").value;
+    video.volume = sliderValue / 100;
+    let volumeInfo = Math.round(video.volume * 100);
+    document.getElementById("volume").textContent = volumeInfo + "%";
+});
+
+document.getElementById("vintage").addEventListener('click', function() {
+    video.classList.add("oldSchool");
+   })
+
+document.getElementById("vintage").addEventListener('click', function() {
+    video.classList.remove("oldSchool");
+   })
+
+});
